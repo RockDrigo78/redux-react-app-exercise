b612064de454c6fd11c1ae2ebd66dda1867b4ba1
RockDrigo78 <rodrigobazandanino@hotmail.com>
2020-05-19 23:46:34 +0200

    Updates

---
+++ precache-manifest.271a191e491f0290367324174789d93e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec9d69003b8150ca7ee2d897598cb98",
+    "url": "/redux-react-app-exercise/index.html"
+  },
+  {
+    "revision": "3a6766818aa5d1734a7b",
+    "url": "/redux-react-app-exercise/static/css/main.5c71d519.chunk.css"
+  },
+  {
+    "revision": "1995d6210bad26dad07f",
+    "url": "/redux-react-app-exercise/static/js/2.1a5ee8df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-react-app-exercise/static/js/2.1a5ee8df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6766818aa5d1734a7b",
+    "url": "/redux-react-app-exercise/static/js/main.c4164757.chunk.js"
+  },
+  {
+    "revision": "7fcf559b38b08db7b093",
+    "url": "/redux-react-app-exercise/static/js/runtime-main.8318df93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-react-app-exercise/static/media/logo.5d5d9eef.svg"
+  }
+]);
